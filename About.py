ef884b85964a31870102ce832ef065ad2065c837
LysanderT <121090088@link.cuhk.edu.cn>
2024-06-23 11:19:23 -0700

    polish description & add new button on About page

--- About.py
+++ About.py
@@ -2,19 +2,28 @@ import streamlit as st
 
 
 def run():
-    st.title("About Planify")
+    # st.title("About Planify")
     st.markdown("""
-    ## 
-    Planify is a comprehensive scheduling and planning tool designed to help you manage your current daily activities, desired added activities, and prioritize your tasks efficiently.
+                
+    ## About Planify
 
-    ### Features
+    Planify is a comprehensive scheduling and planning tool designed to help you manage your current weekly activities, add new tasks, and prioritize your responsibilities efficiently.
+
+    #### Features
     - **Initial**: Manually schedule your established activities or import an existing schedule with specific start and end times for each day of the week.
     - **Desired**: Add additional tasks and events with a focus on their importance and duration.
     - **Preferences**: Update and set your preferences for night work and extra breaks to customize your schedule.
     - **Plan**: View your created schedule! Also has a summarized JSON format of your planned activities and tasks.
 
-    ### Developers
-    Planify was developed by Lysander, Lakshya and Shuhan focused on providing the best user experience for managing daily tasks and schedules. 
+    #### Developers
+    Planify was developed by Lysander, Lakshya, and Shuhan with a focus on providing the best user experience for managing daily tasks and schedules.
 
     We hope you find Planify helpful in organizing your daily activities and tasks!
+
     """)
+
+    # col1, _, _, col4 = st.columns(4)
+    _, col2, _ = st.columns(3,vertical_alignment="center", gap = "large")
+    if col2.button("Start Here!"):
+        st.session_state.page = "Initial"
+        st.experimental_rerun()
